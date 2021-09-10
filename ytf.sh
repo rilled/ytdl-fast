1113e0916c85f0c07ac716e04db954d4b38010be
rilled <rilled@pm.me>
2021-09-10 23:30:16 +0000

    Major cleanup, improved systems and start of backup import system

--- ytf.sh
+++ ytf.sh
@@ -1,18 +1,11 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # usage: yt (video|playlist|channel)
-
-Color_Off='\033[0m' # Text Reset
-
-# Regular Colors
-Black='\033[0;30m'  # Black
-Red='\033[0;31m'    # Red
-Green='\033[0;32m'  # Green
-Yellow='\033[0;33m' # Yellow
-Blue='\033[0;34m'   # Blue
-Purple='\033[0;35m' # Purple
-Cyan='\033[0;36m'   # Cyan
-White='\033[0;97m'  # White
+Green='\e[32m'
+Red='\e[31m'
+Cyan='\e[36m'
+White='\e[97m'
+Reset='\e[0m'
 
 # YTDL
 # codes: https://voussoir.net/writing/youtubedl_formats
@@ -22,7 +15,24 @@ White='\033[0;97m'  # White
 # -c > continue
 # --write-sub OR --write-auto-sub
 
-function ffmpegCheck() {
+DIR=""
+
+function stripImport {
+  read -p ${Cyan}"Enter desired file to import:"${Color_Off} file
+  sed -i -r 's/(.{11}).*/\1/' ${file} # remove all fluf after video code, YT videos are always 11chars
+  sed -i -e 's#^#https://youtube.com/watch?v=#' ${file} # append url prefix
+  echo -e ${Green}"Import complete!"
+}
+
+function ytdlCheck {
+  if dpkg -l youtube-dl >/dev/null; then
+    return
+  else
+    echo -e "${Red} [!] youtube-dl not found, installing... ${Color_Off}"
+    sudo apt-get install youtube-dl -y
+}
+
+function ffmpegCheck {
   if dpkg -l ffmpeg >/dev/null; then
     return
   else
@@ -32,63 +42,55 @@ function ffmpegCheck() {
 }
 
 function selectQuality(media, mediaType) {
-  case $mediaType in
-    video)
-    ;;
-    playlist)
-    ;;
-    channel)
+  case ${mediaType} in
+    video | playlist | channel)
+      echo -ne "${Green} ##############################
+      ${Cyan} Select quality for download:
+      ${White} [1] Low-res (360p) # 18
+      ${White} [2] High-res (720p) # 22
+      ${White} [3] Audio-only # 140
+      ${White} [4] Best possible quality"
+      read -p "${Green} ############################## ${Color_Off}" quality
+      download(${media},${quality}) ;;
+    *)
+      echo ${Red}"Invalid input ${MediaType}"${Color_Off}
+      exit 1 ;;
   esac
 }
 
-function getMedia() {
-  echo -e "${Cyan} Enter dir for media: ${Color_Off}"
-  read dir
-  mkdir $dir
-  echo -e "${Cyan} Entering '$dir' ${Color_Off}"
-  cd "$dir" && pwd
-  echo -e "${Green} ############################## ${Color_Off}"
+function download(media, quality, extras) { # todo: add support for thumbnails, desc and subs
+  case ${quality} in
+    1)
+      youtube-dl -f 18 -ciw -o "%(title)s.%(ext)s" -i ${c} ;;
+    2)
+      youtube-dl -f 22 -ciw -o "%(title)s.%(ext)s" -i ${c} ;;
+    3)
+      youtube-dl -f 140 -ciw -o "%(title)s.%(ext)s" -i ${c} ;;
+    4)
+      youtube-dl -f 'bestvideo,bestaudio' -ciw -o "%(title)s.%(ext)s" -i ${c} ;;
+  esac
+}
 
-  if [[ ${1} == "video" ]]; then
-    echo -e "${Cyan} Enter video URL to save: ${Color_Off}"
-    read -r c
-    echo -e "${Green} Starting YTDL Service... ${Color_Off}"
-    youtube-dl -f 'bestvideo,bestaudio' -ciw -o "%(title)s.%(ext)s" -i ${c}
+function start() {
+  ffmpegCheck
+  ytdlCheck
 
-  elif [[ ${1} == "playlist" ]]; then
-    echo -e "${Cyan} Enter Playlist URL to save: ${Color_Off}"
-    read -r c
-    selectQuality(${c},${1}) # OKAYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYY
+  if [ ${2} == "-d" ] then
+    read -p ${Cyan}"Enter dir for media:"${Color_Off} dir
+    mkdir $dir
+    echo -e ${Cyan}"Entering '$dir'"${Color_Off} && cd "$dir"
+    DIR="${dir}"
+  else
+    echo -e ${Cyan}"Using current directory ${White} (use '-d' to specify directory)"${Color_Off}
+    DIR=$(pwd)
+  fi
 
-    echo -e "${Green} ##############################"
-    echo -e "${Cyan} Select quality for entire playlist:"
-    echo -e "${Cyan} [1] Low-res (360p)" # 18
-    echo -e "${Cyan} [2] High-res (720p)" # 22
-    echo -e "${Cyan} [3] Audio-only" # 140
-    echo -e "${Cyan} [4] Best possible quality ${Color_Off}"
-    read -r q
-
-    case ${q} in
-      1)
-      youtube-dl -f 18 -ciw -o "%(title)s.%(ext)s" -i ${c}
-      ;;
-      2)
-      youtube-dl -f 22 -ciw -o "%(title)s.%(ext)s" -i ${c}
-      ;;
-      3)
-      youtube-dl -f 140 -ciw -o "%(title)s.%(ext)s" -i ${c}
-      ;;
-      4)
-      youtube-dl -f 'bestvideo,bestaudio' -ciw -o "%(title)s.%(ext)s" -i ${c}
-    esac
-
-  elif [[ ${1} == "channel" ]]; then
-    echo -e "${Cyan} Enter Channel URL to save: ${Color_Off}"
-    read -r c
-    echo -e "${Green} Starting YTDL Service... ${Color_Off}"
-    youtube-dl -f 'bestvideo,bestaudio' -ciw -o "%(title)s.%(ext)s" -v ${c}
+  if [[ ${1} == "video" | "playlist" | "channel" ]]; then
+    read -p ${Cyan}"Enter URL to save:"${Color_Off} c
+    selectQuality(${c},${1})
+  else
+    echo ${Red}"Invalid selection."${Color_Off}
   fi
 }
 
-ffmpegCheck
-getMedia
+start
